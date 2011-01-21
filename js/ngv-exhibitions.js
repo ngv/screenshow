4ed88668e7a47796da15d1bed7d514934874687e
Maksim Lin <maksim.lin@ngv.vic.gov.au>
2011-01-21 15:55:22 +1100

    added skeleton main config and ngv plugins.

---
+++ js/ngv-exhibitions.js
@@ -0,0 +1,13 @@
+/**
+ * A screenshow plugin to display in-house exhibition slides from the NGV private data feed:
+ * http://www.ngv.vic.gov.au/json-feeds/exhibition-slides-json
+ */
+
+
+/**
+ * Callback which will be run by screenshow to initialise the plugin with the
+ * cofig data as specified in the screenshow main config file
+ */
+function init(config) {
+
+}
---
+++ js/ngv-whatson.js
@@ -0,0 +1,24 @@
+/**
+ * A screenshow plugin to display whats on today information from the NGV public WhatsOn data feed:
+ * http://www.ngv.vic.gov.au/xml-feeds/whats-on.xml
+ */
+
+
+/**
+ * Callback which will be run by screenshow to initialise the plugin with the
+ * cofig data as specified in the screenshow main config file
+ */
+function init(config) {
+
+}
+
+/**
+ * Callback to
+ * @returns {Array} An array of objects, each object represents 1 slide to be displayed in the
+ * slide show. Each object is expected to have the following properties:
+ *      duration {Number} the time in seconds for the slide
+ *      html {String} the content of the slide
+ */
+function buildSlides() {
+
+}
